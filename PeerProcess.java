60d91259e02082488957de239fbcdcb16b13ba59
Neha Sharma <nehasharma29@outlook.com>
2020-10-21 18:02:22 -0400

    enter in client names through args

--- PeerProcess.java
+++ PeerProcess.java
@@ -4,9 +4,9 @@ public class PeerProcess {
 
     public static void main(String[] args) throws IOException{
         Server server = new Server(15123);
-        Client client1 = new Client(15123, "client1");
-        Client client2 = new Client(15123, "client2");
-        Client client3 = new Client(15123, "client3");
+        Client client1 = new Client(15123, args[0]);
+        Client client2 = new Client(15123, args[1]);
+        Client client3 = new Client(15123, args[2]);
 
     }
     
